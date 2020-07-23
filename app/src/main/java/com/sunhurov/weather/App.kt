307e6007cbf13b7e19bcca57e2a6e055ca4e155b
Yurii Sunhurov <yurii.sunhurov@rfidsolutions.pl>
2020-07-23 09:19:39 +0200

    add buildSrc module, ktlint, detekt

---
+++ app/src/main/java/com/sunhurov/weather/App.kt
@@ -0,0 +1,14 @@
+package com.sunhurov.weather
+
+import android.app.Application
+
+/**
+ * Created by Yurii Sunhurov on 23.07.2020
+ */
+
+open class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,102 @@
+/**
+ * Created by Yurii Sunhurov on 23.07.2020
+ */
+
+
+object ApplicationId {
+    val id = "com.sunhurov.weather"
+}
+
+object Modules {
+    val app = ":app"
+
+    val navigation = ":navigation"
+
+    val common = ":common"
+
+    val local = ":data:local"
+    val remote = ":data:remote"
+    val model = ":data:model"
+    val repository = ":data:repository"
+
+    val featureHome = ":features:home"
+    val featureDetail = ":features:detail"
+    val featureSearch = ":features:search"
+}
+
+
+object Releases {
+    val versionCode = 1
+    val versionName = "1.0"
+}
+
+object Versions {
+    val kotlin = "1.3.72"
+    val gradle = "4.0.1"
+    val compileSdk = 29
+    val minSdk = 23
+    val targetSdk = 29
+    val appCompat = "1.1.0"
+    val multidex = "1.0.3"
+    val coreKtx = "1.2.0"
+    val constraintLayout = "1.1.3"
+    val junit = "4.12"
+    val androidTestRunner = "1.1.2-alpha02"
+    val espressoCore = "3.2.0-alpha02"
+    val retrofit = "2.8.1"
+    val retrofitCoroutines = "0.9.2"
+    val retrofitGson = "2.8.1"
+    val gson = "2.8.6"
+    val okHttp = "3.11.0"
+    val coroutines = "1.3.6"
+    val koin = "2.1.5"
+    val timber = "4.7.1"
+    val lifecycle = "2.2.0"
+    val nav = "2.3.0-alpha06"
+    val room = "2.2.5"
+    val recyclerview = "1.1.0"
+    val safeArgs = "2.3.0"
+    val glide = "4.11.0"
+    val databinding = "3.3.2"
+}
+
+object Libraries {
+    // KOIN
+    val koin = "org.koin:koin-android:${Versions.koin}"
+    val koinViewModel = "org.koin:koin-android-viewmodel:${Versions.koin}"
+    // ROOM
+    val roomCompiler = "androidx.room:room-compiler:${Versions.room}"
+    val roomRunTime = "androidx.room:room-runtime:${Versions.room}"
+    val roomKtx = "androidx.room:room-ktx:${Versions.room}"
+    // RETROFIT
+    val retrofitCoroutineAdapter = "com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:${Versions.retrofitCoroutines}"
+    val gson = "com.google.code.gson:gson:${Versions.gson}"
+    val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    val retrofitGsonConverter = "com.squareup.retrofit2:converter-gson:${Versions.retrofitGson}"
+    val httpLoggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okHttp}"
+    val okHttp = "com.squareup.okhttp3:okhttp:${Versions.okHttp}"
+    // GLIDE
+    val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
+    val glideCompiler = "com.github.bumptech.glide:compiler:${Versions.glide}"
+}
+
+object KotlinLibraries {
+    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    val kotlinCoroutineCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
+}
+
+object AndroidLibraries {
+    // KOTLIN
+    val kotlinCoroutineAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
+    // ANDROID
+    val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    val multidex  = "com.android.support:multidex:${Versions.multidex}"
+    val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+    val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    val lifecycleViewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
+    val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle}"
+    val lifecycleCompiler = "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycle}"
+    val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerview}"
+    val navigation = "androidx.navigation:navigation-ui-ktx:${Versions.nav}"
+    val navigationFrag = "androidx.navigation:navigation-fragment-ktx:${Versions.nav}"
+}
